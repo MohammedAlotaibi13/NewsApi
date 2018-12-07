052d4c71e162bbdfbc29b937ced2d1db29025937
MohammedAlotaibi13 <muhammedalotaibi13@gmail.com>
2018-12-07 21:52:03 +0300

    delete unnecessary parameters `

--- ApiNews/Model/Constant.swift
+++ ApiNews/Model/Constant.swift
@@ -10,16 +10,7 @@ import Foundation
 
 struct Constant {
     struct NewsUrl {
-        static let url = "https://newsapi.org/v2/top-headlines?"
+        static let url = ""
     }
     
-    struct ParametersKey {
-        static let apiKey = "apiKey"
-        static let source = "sources"
-    }
-    
-    struct ParametersValue {
-        static let apiKey = "f8c65cfeea364fdb99bb9ecadc5bbd07"
-        static let source = "cnn"
-    }
 }
